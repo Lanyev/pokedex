9372e2a4053aee165980c7bdd4ed350bd4622949
Alan Yeverino <lanyev@gmail.com>
2022-12-17 21:34:32 -0600

    Modo CRT agregado

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import FormHome from "../components/FormHome";
 import Footer from "../layout/Footer";
 import "../styles/Home.css";
 
 const Home = () => {
+  // Inicializa el estado showAnimation en true
+  const [showAnimation, setShowAnimation] = useState(false);
+
+  // Agrega un manejador de evento al botón para cambiar el estado showAnimation
+  const handleToggleAnimation = () => {
+    setShowAnimation(!showAnimation);
+  };
+
   return (
     <main className="home">
       <div className="home__img-container">
         <img className="home__imgP" src="/images/p.png" alt="" />
-        <img className="home__imgO" src="/images/pokeball.png" alt="" />
+        <img
+          className="home__imgO"
+          src="/images/pokeball.png"
+          alt=""
+          onClick={handleToggleAnimation}
+        />
         <img className="home__imgKedex" src="/images/kedex.png" alt="" />
       </div>
-      <div className="home__name">
+      <div className={`home__name ${showAnimation ? "" : "no-animation"}`}>
         <h2 className="home__subtitle">Hi, trainer!</h2>
         <p className="home__text">
           Give me your name <span> to start your journey!</span>
         </p>
-        <FormHome />
-        <Footer />
       </div>
+      <FormHome />
+      <Footer />
     </main>
   );
 };
