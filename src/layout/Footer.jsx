63cff772f09d3521a8b3b88a4b34308e59096c64
Alan Yeverino <lanyev@gmail.com>
2022-12-17 20:05:52 -0600

    PIKACHUUUUU

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -4,6 +4,9 @@ import "../styles/Footer.css";
 const Footer = () => {
   return (
     <footer className="footer">
+      <div className="footer__img">
+        <img className="pikachu" src="/images/pika.gif" alt="" />
+      </div>
       <div className="footer__black"></div>
       <div className="footer__white"></div>
       <p className="footer__text">© 2022 - Made by Alan Yeverino</p>
