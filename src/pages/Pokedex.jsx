a8c46dddcf1a3aba907dad88105105ca9306c7a9
Alan Yeverino <lanyev@gmail.com>
2022-12-17 19:46:50 -0600

    Mejoras de pantalla

--- src/pages/Pokedex.jsx
+++ src/pages/Pokedex.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 
 const Pokedex = () => {
+  const [pokemon, setPokemon] = useState([]);
   const nameTrainer = useSelector((state) => state.nameTrainer);
+
+  useEffect(() => {
+    const URL = "https://pokeapi.co/api/v2/pokemon?limit=1154";
+    axios
+      .get(URL)
+      .then((response) => setPokemon(response.data))
+      .catch((error) => console.log(error));
+  }, []);
 
   return (
     <main>
